b89fc5116a7dab935856271eac356144df5099ac
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-04-15 15:27:39 +0100

    deploy vercelll render

--- Backend/main.py
+++ Backend/main.py
@@ -17,7 +17,7 @@ load_dotenv()
 app = create_app()
 
 # Configure CORS from environment variables
-allowed_origins = os.getenv('ALLOWED_ORIGINS', 'http://localhost:8080').split(',')
+allowed_origins = os.getenv('ALLOWED_ORIGINS', 'https://live-stream-monitoring-vue3-flask.vercel.app').split(',')
 CORS(app, supports_credentials=True, origins=allowed_origins)
 
 # Configure logging
