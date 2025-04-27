92d1c4fb48902cb5591cbff836817436c8dc41be
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-04-27 15:47:44 +0100

    Update vue.config.js

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,15 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/', // keep as is
-  
-  productionSourceMap: false, // Disable sourcemaps in production for faster build & better security
-
-  configureWebpack: {
-    infrastructureLogging: {
-      level: 'warn', // Only show important Webpack logs
-    },
-    bail: true, // Critical: stop build on first error
-  }
+  publicPath: '/', 
 })
