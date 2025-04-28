1e5056ca0a590f308e7f18e417dca452f8fe7728
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-04-28 16:57:08 +0100

    semi-finished: almost there

--- Frontend/src/composables/useRealTimeDetection.js
+++ Frontend/src/composables/useRealTimeDetection.js
@@ -28,7 +28,7 @@ export function useRealTimeDetection() {
    * @param {Object} options.preferences - Notification preferences
    */
   const initialize = ({ 
-    serverUrl = 'http://54.86.99.85:5000', 
+    serverUrl = 'https://54.86.99.85:5000', 
     autoConnect = true,
     preferences = {}
   } = {}) => {
