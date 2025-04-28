317fc378762ed03dd4a5b013f61295f471e1111b
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-04-28 23:51:20 +0100

    semi-finished: almost there

--- backend/main.py
+++ backend/main.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host='0.0.0.0',
-        port=int(os.getenv('PORT', 443)),
+        port=int(os.getenv('PORT', 8080)),
         debug=os.getenv('FLASK_DEBUG', 'false').lower() == 'false',
         use_reloader=False,
         ssl_context=ssl_ctx
