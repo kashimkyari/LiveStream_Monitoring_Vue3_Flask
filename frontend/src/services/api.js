c9b5baa4f1a1781deced95345b5c25ef18c5160f
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-05-16 17:54:57 +0100

    refactored stream card/dashboardtab

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: ' http://localhost:5000',
+  baseURL: ' https://monitor-backend.jetcamstudio.com:5000',
   withCredentials: true,               // Include cookies/credentials
   headers: {
     'Content-Type': 'application/json'
